f38f249242ccd041f07e2a79a49065146d8fc076
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-09-23 12:04:48 +0600

    feat: Add admin page design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,8 @@
+import Announcements from "@/components/Announcements";
 import AttendenceChart from "@/components/AttendenceChart";
 import CountCharts from "@/components/CountCharts";
+import EventCalendar from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -7,29 +10,34 @@ const AdminPage = () => {
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* left */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
-        {/* user cards */}
+        {/* USER CARDS */}
         <div className="flex flex-wrap gap-4 justify-between">
           <UserCard type="students" />
           <UserCard type="teachers" />
           <UserCard type="parents" />
           <UserCard type="staffs" />
         </div>
-        {/* middle charts */}
+        {/* MIDDLE CHARTS */}
         <div className="flex flex-col lg:flex-row gap-4">
-          {/* count chart */}
+          {/* COUNT CHART */}
           <div className="w-full lg:w-1/3 h-[450px]">
             <CountCharts />
           </div>
-          {/* attendence chart */}
+          {/* ATTENDENCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendenceChart />
           </div>
         </div>
-        {/* bottom chart */}
-        <div className="w-full h-[500px]"></div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
-      {/* right */}
-      <div className="w-full lg:w-1/3">r</div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,9 +1,17 @@
+import Announcements from "@/components/Announcements";
+import EventCalendar from "@/components/EventCalendar";
 import React from "react";
 
 const StudentPage = () => {
   return (
-    <div>
-      <h1>Student</h1>
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3"></div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
   );
 };
